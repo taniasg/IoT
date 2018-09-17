709131973bf706272ef2acb2c66f928b21302624
Tania Sanchez <tania.sanchez16@live.com>
2018-09-17 15:40:57 -0500

    Se modifica el componente para recibir el dato emitido por el socket y se almacena en el array principal

--- frontend/src/app/sensor/sensor.component.ts
+++ frontend/src/app/sensor/sensor.component.ts
@@ -20,70 +20,64 @@ export class SensorComponent implements OnInit {
   private hum: Array<any>;
   private temp: Array<any>;
 
+  private dataChartHum: Array<any>;
+  private dataChartTemp: Array<any>;
+
   private currentHum;
   private currentTemp;
 
   private response: boolean;
 
   constructor(private _sensorService: SensorService) {
-
   }
 
   ngOnInit() {
+  	this.obtenerHumedad();
+    this.obtenerTemperatura();
 
     const socket = SocketIO('http://localhost:8000');
 
     socket.on('data', (data) => {
-      if(data.change) {
-        this.obtenerHumedad();
-        this.obtenerTemperatura();
-      }
+    	if(this.response){
+    		let date = new Date(Date.now());
+    		if(data.data.sensor == "HUM") {
+    			this.currentHum = { 
+    				'data': data.data.data, 
+    				'value': data.data.value.toFixed(2), 
+    				'date': date
+    			};
+    			
+    			this.hum.push(this.currentHum);
+    			this.dataChartHum = this.createDataChart(this.hum);
+    			this.createChartHum();
+
+    		}
+    		if(data.data.sensor == "TEMP") {
+    			this.currentTemp = { 
+    				'data': data.data.data, 
+    				'value': data.data.value.toFixed(2), 
+    				'date': date
+    			};
+
+    			this.temp.push(this.currentTemp);
+    			this.dataChartTemp = this.createDataChart(this.temp);
+    			this.createChartTemp();  
+    		}
+    	}
     });
-  	
   }
 
   ngDoCheck() {
-    if(this.hum && this.temp && !this.response) {
-      this.response = true;
+    if(this.temp && this.hum && !this.response) {
+      this.response = true; 
     }
   }
 
   obtenerHumedad() {
     this._sensorService.obtenerHumedad().subscribe(
       res => {
-        this.hum = this.generateArray(res);
-
-        res[res.length - 1 ].date = new Date(res[res.length - 1].date);
-        res[res.length - 1].value = res[res.length - 1].value.toFixed(2);
-
-        this.currentHum = res[res.length - 1];
-
-        if(this.response) {
-          this.chartHumidity = new Chart('humidity', {
-            type: 'line',
-            data: {
-              labels:this.hum['labels'],
-              datasets: [{
-                label: 'Ultimos 15 minutos',
-                data: this.hum['data'],
-                fill: false,
-                lineTension: 0.2,
-                borderColor: "#549BE2",
-                borderWidth: 3
-              }]
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 0,
-                        max: 80
-                    }
-                }]
-              }
-            }
-          });
-        }        
+        this.hum = res;  
+        this.dataChartHum = this.createDataChart(this.hum); 
     },
       error => {
         console.log(<any>error);
@@ -93,61 +87,85 @@ export class SensorComponent implements OnInit {
   obtenerTemperatura() {
     this._sensorService.obtenerTemperatura().subscribe(
       res => {
-        this.temp = this.generateArray(res);
-
-        res[res.length - 1 ].date = new Date(res[res.length - 1].date);
-        res[res.length - 1].value = res[res.length - 1].value.toFixed(2);
-        this.currentTemp = res[res.length - 1];
-
-        if(this.response) {
-          this.chartTemperature = new Chart('temperature', {
-            type: 'line',
-            data: {
-              labels:this.temp['labels'],
-                datasets: [{
-                label: 'Ultimos 15 minutos',
-                data: this.temp['data'],
-                fill: false,
-                lineTension: 0.2,
-                borderColor: "#549BE2",
-                borderWidth: 3
-              }]
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 0,
-                        max: 30
-                    }
-                }]
-              }
-            }
-          });
-        }
+        this.temp = res;
+        this.dataChartTemp = this.createDataChart(this.temp);
     },
       error => {
         console.log(<any>error);
     });
   }
 
-  generateArray(res): Array<any> {
+  createChartTemp() {
+  	this.chartTemperature = new Chart('temperature', {
+        type: 'line',
+        data: {
+          labels:this.dataChartTemp['labels'],
+            datasets: [{
+            label: 'Ultimos 15 minutos',
+            data: this.dataChartTemp['data'],
+            fill: false,
+            lineTension: 0.2,
+            borderColor: "#549BE2",
+            borderWidth: 3
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 30
+                }
+            }]
+          }
+        }
+      });
+  }
+
+  createChartHum() {
+    this.chartHumidity = new Chart('humidity', {
+        type: 'line',
+        data: {
+          labels:this.dataChartHum['labels'],
+          datasets: [{
+            label: 'Ultimos 15 minutos',
+            data: this.dataChartHum['data'],
+            fill: false,
+            lineTension: 0.2,
+            borderColor: "#549BE2",
+            borderWidth: 3
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 80
+                }
+            }]
+          }
+        }
+    });    
+  }
+
+  createDataChart(arr): Array<any> {
     let sensor = [];
     sensor['data'] = [];
     sensor['labels'] = [];
     
-    let intervalo = parseInt((res.length/6 + 1).toFixed(0));
+    let intervalo = parseInt((arr.length/6 + 1).toFixed(0));
 
-    for(let i = 0; i < res.length; i += intervalo){
-      res[i].date = new Date(res[i].date);
-      sensor['data'].push(res[i].value);
-      sensor['labels'].push(res[i].date.toLocaleTimeString());
+    for(let i = 0; i < arr.length; i += intervalo){
+      arr[i].date = new Date(arr[i].date);
+      sensor['data'].push(arr[i].value);
+      sensor['labels'].push(arr[i].date.toLocaleTimeString());
     }
 
-    sensor['data'].push(res[res.length - 1].value);
+    sensor['data'].push(arr[arr.length - 1].value);
 
-    res[res.length - 1 ].date = new Date(res[res.length - 1].date);
-    sensor['labels'].push(res[res.length - 1].date.toLocaleTimeString());
+    arr[arr.length - 1 ].date = new Date(arr[arr.length - 1].date);
+    sensor['labels'].push(arr[arr.length - 1].date.toLocaleTimeString());
 
     return sensor;
 
